9f1f440c368bc54ad4244de1a94e9e6eaaebcf3d
Pedro Jesus <pedrojesus@192.168.1.2>
2024-11-16 17:46:18 -0300

    inicio : configura git -sync

--- airflow/main.tf
+++ airflow/main.tf
@@ -9,14 +9,20 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
+
+variable "variables" {
+  default = "credentials/env.json"
+}
+
 # Define the path to the credentials file
-variable "credentials_file" {
-  default = "credentials_file_aws/terraform_user_acessKeys.json"
+variable "terraform_credentials_file" {
+  default = "credentials/terraform_user_acessKeys.json"
 }
 
 # Read and parse the JSON config file
 locals {
-  credentials_data = jsondecode(file(var.credentials_file))
+  credentials_data = jsondecode(file(var.terraform_credentials_file))
+  variables = jsondecode(file(var.variables))
 }
 
 # Configure the AWS provider using the parsed values
@@ -39,7 +45,8 @@ resource "aws_instance" "app_server" {
     market_type = "spot"
     spot_options {
       spot_instance_type = "one-time"
-      instance_interruption_behavior = "terminate" # Behavior when interrupted: "terminate", "stop", or "hibernate"
+      instance_interruption_behavior = "terminate" 
+      # Behavior when interrupted: "terminate", "stop", or "hibernate"
     }
   }
 
@@ -56,6 +63,58 @@ resource "aws_instance" "app_server" {
     Name = "airflow-instance"
   }
 
-  user_data = file("init.sh")
+  provisioner "file" {
+    source      = "docker-compose.yaml"  # Path to the local file
+    destination = "./docker-compose.yaml"  # Path on the EC2 instance
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "ls -l ./docker-compose.yaml",  # Verify the file was uploaded
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = file("/Users/pedrojesus/key_pair_ec2_aws.pem")  # Path to your private key
+  }
+
+  user_data = <<-EOF
+    #!/bin/bash
+
+    #    # INSTALL DOCKER
+    #    sudo apt-get update -y
+    #    sudo apt-get install ca-certificates curl -y
+    #    sudo install -m 0755 -d /etc/apt/keyrings
+    #    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    #    sudo chmod a+r /etc/apt/keyrings/docker.asc
+    #    echo \
+    #      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    #      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    #      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    #    sudo apt-get update -y
+    #    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+    #    sudo groupadd docker
+    #    sudo gpasswd -a $USER docker
+    #    sudo service docker restart
+    #    sudo docker context use default
+    #
+    #    # INSTALL APACHE AIRFLOW
+    #    mkdir airflow
+    #    mv docker-compose airflow
+    #    cd airflow
+    #    mkdir -p ./dags ./logs ./plugins
+    #    echo -e "AIRFLOW_UID=$(id -u)" > .env
+    #    AIRFLOW_UID=50000
+    #    sudo docker compose up airflow-init
+    #    sudo docker compose up -d
+
+    # Set environment variable permanently in the user's shell profile
+    echo 'export GIT_SYNC_REPO=${local.variables["GIT_SYNC_REPO"]}' >> ~/.bashrc
+    source ~/.bashrc
+
+  EOF
   
 }
